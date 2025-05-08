b0fc0e254228ff4275e5796ac1bdac65af8f84cd
Aji Kamaludin <aji19kamaludin@gmail.com>
2025-05-08 14:34:58 +0700

    remove other project resources

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\ItemHistoryController;
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\PurchaseController;
-use App\Http\Controllers\Api\SaleController;
 use App\Http\Controllers\Default\Api\SelectTableController;
 use App\Http\Controllers\Default\FileController;
 use App\Http\Middleware\JwtCustomApiVerification;
@@ -18,9 +14,5 @@ Route::middleware([JwtCustomApiVerification::class])
     ->prefix('_default')
     ->group(function () {
         Route::get('/select/{table}', SelectTableController::class)->name('api.select.table');
-        Route::get('item-histories', [ItemHistoryController::class, 'index'])->name('api.item-history.index');
-        Route::get('/sales', [SaleController::class, 'index'])->name('api.sale.index');
-        Route::get('/purchases', [PurchaseController::class, 'index'])->name('api.purchase.index');
-        Route::get('/products', [ProductController::class, 'index'])->name('api.product.index');
         Route::post('files', [FileController::class, 'store'])->name('api.file.store');
     });
